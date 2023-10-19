1bf136776a357a3ba066eea73632c27e2aaa4af7
Alok Desai <alokdesai2004@gmail.com>
2023-10-19 17:44:46 +0530

    beautify dashboard

--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -17,7 +17,7 @@ export default function DashBoard() {
   return (
     <div>
       <Logo/>
-      <h1>DashBoard</h1>
+      {/* <h1>DashBoard</h1>
       <p>Welcome - {`${user.firstName} ${user.lastName?user.lastName:""}`}</p>
       <div>
         <img src={user.pictureUrl} alt="" className="
@@ -25,6 +25,36 @@ export default function DashBoard() {
         "/>
         </div>
       <p>user: {JSON.stringify(user)}</p>
+      <PillButton children="Products" onClick={handleClick}/> */}
+      <div className="grid grid-cols-2 m-2 bg-con-blue content-center rounded-3xl p-5 border border-gray-400">
+        <div>
+          <img src={user.pictureUrl} alt=""
+            className="
+              rounded-full
+              w-1/3
+            "/>
+        </div>
+        <div className="grid grid-rows-1 justify-start gap-1">
+        <div className="text-3xl">
+          <span className="font-light">Welcome, </span>
+          <span className="font-semibold">{user.firstName}{user.lastName?user.lastName:""}!</span>
+        </div>
+        <div>
+          {user.email}
+        </div>
+        <div>
+          <span className="font-semibold">Purchases: </span><span>{null}</span>
+        </div>
+        <div>
+          <span className="font-semibold">Wallet Balance: </span><span>{null}</span>
+        </div>
+        <div>
+        <PillButton children="Add Balance" 
+                className={"py-2 px-8 bg-regal-blue border border-gray-600 text-gray-600 hover:text-gray-500"}
+                />
+        </div>
+      </div>
+      </div>
       <PillButton children="Products" onClick={handleClick}/>
     </div>
   );
